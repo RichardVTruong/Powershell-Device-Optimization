f076454744f8a077d4b112ce424c9cbb9ce8c518
Richard Truong <122242783+RichardVTruong@users.noreply.github.com>
2024-02-22 18:19:55 -0800

    Add files via upload

---
+++ DellCommandUpdate_Scan-ApplyUpdates.ps1
@@ -0,0 +1,4 @@
+# Executes the Dell Command Update scan function
+"C:\Program Files\Dell\CommandUpdate\dcu-cli.exe" /scan
+# Executes the Dell Command Update apply updates function
+"C:\Program Files\Dell\CommandUpdate\dcu-cli.exe" /applyUpdates
